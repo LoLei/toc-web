5fa265ece2225831dd624808e3d2176344c67030
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-02-14 17:54:08 +0100

    Update CSS

--- tocweb.py
+++ tocweb.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def my_form():
-    return render_template('my-form.html')
+def form():
+    return render_template('form.html')
 
 
 @app.route('/', methods=['POST'])
-def my_form_post():
+def form_post():
     text = request.form['text']
     processed_text = text.upper()
     return render_template('result.html', result=processed_text)
